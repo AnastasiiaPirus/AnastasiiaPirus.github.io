46371885f35143a9e4e16394989275e632888d8d
AnastasiiaPirus <a.pirus2000@gmail.com>
2023-01-19 15:22:14 -0500

    Updated Route path="/Portfolio"

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} exact /> 
+        <Route path="/Portfolio" element={<Home />} exact /> 
         
       </Routes>
 
